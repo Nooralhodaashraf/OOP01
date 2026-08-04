3a87e0947ded77f6d9a9bbedb3ab8285a3031ae5
Nooralhodaashraf <nooralhodaashraf@gmail.com>
2026-08-05 02:38:09 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Assignment01_OOP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
