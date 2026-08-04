f2b8971ca312d87dc2a8ba9170b3008c5848a84a
Nooralhodaashraf <nooralhodaashraf@gmail.com>
2026-08-05 02:50:10 +0300

    part 01 Q01 a

--- Program.cs
+++ Program.cs
@@ -4,7 +4,20 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Part 001 Q01 a)
+            //a)	 What happens when a DeliveryAddress variable is copied into another variable and the copy is modified?
+            // it wont effect the original variable because it is a struct and structs are value types so it creates a new copy of the variable
+        //DeliveryAddress address01 = new DeliveryAddress();
+        //    address01.City = "Cairo";
+        //    address01.Street = "dokki";
+        //    DeliveryAddress address02 = new DeliveryAddress();
+        //    address02 = address01;
+        //    address02.City = "Giza";
+        //    Console.WriteLine(address02.City);// Giza
+        //    Console.WriteLine(address01.City);// Cairo
+            //not the same 
+
+            #endregion
         }
     }
 }
---
+++ struct/DeliveryAddress.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Assignment01_OOP
+{
+    internal struct DeliveryAddress
+    {
+        public string City;
+        public string Street;
+    }
+
+    public class Customer
+    {
+        public string Name;
+    }
+
+}
